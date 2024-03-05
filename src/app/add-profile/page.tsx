7f4b313650e4457d8bbdac90ec9038122010a48f
oyinkansola <yahayaoyinkansola@gmail.com>
2024-03-05 14:54:32 +0100

    Update add profile UI to a multistep form

--- src/app/add-profile/page.tsx
+++ src/app/add-profile/page.tsx
@@ -1,6 +1,19 @@
+"use client";
+
+import { useState } from "react";
 import Link from "next/link";
 
 export default function Page() {
+  const [step, setStep] = useState(1);
+
+  function handlePrev() {
+    setStep((prevStep) => prevStep - 1);
+  }
+
+  function handleNext() {
+    setStep((prevStep) => prevStep + 1);
+  }
+
   return (
     <section>
       <div className="container">
@@ -9,79 +22,213 @@ export default function Page() {
             &laquo; Back
           </Link>
 
-          <div className="contact">
-            <form
-              action="#"
-              method="post"
-              className="contact-form mt-30 mb-30"
-              id="contact-form"
-            >
-              <div className="row row-30">
-                <div className="col-lg-6">
-                  <div className="form-group">
-                    <label htmlFor="name">Name</label>
+          <form
+            action="#"
+            method="post"
+            className="contact-form mt-30 mb-30"
+            id="contact-form"
+          >
+            <div className="contact">
+              {step === 1 && (
+                <>
+                  <h3 className="font-bold mb-3 text-lg">
+                    General Information
+                  </h3>
 
-                    <input
-                      type="text"
-                      className="form-control"
-                      name=""
-                      id="name"
-                    />
+                  <div className="row row-30">
+                    <div className="col-lg-6">
+                      <div className="form-group">
+                        <label htmlFor="fname">Full Name</label>
+
+                        <input
+                          type="url"
+                          className="form-control"
+                          name="fname"
+                          id="fname"
+                        />
+                      </div>
+                    </div>
+
+                    <div className="col-lg-6">
+                      <div className="form-group">
+                        <label htmlFor="sex">Gender</label>
+
+                        <select name="sex" id="" className="form-control">
+                          <option value="male">Male</option>
+                          <option value="female">Female</option>
+                        </select>
+                      </div>
+                    </div>
+
+                    <div className="col-lg-6">
+                      <div className="form-group">
+                        <label htmlFor="dob">Date of Birth</label>
+
+                        <input
+                          type="date"
+                          name=""
+                          id="dob"
+                          className="form-control"
+                        />
+                      </div>
+                    </div>
+
+                    <div className="col-lg-6">
+                      <div className="form-group">
+                        <label htmlFor="phone">Phone number</label>
+
+                        <input
+                          type="tel"
+                          className="form-control"
+                          name=""
+                          id="phone"
+                        />
+                      </div>
+                    </div>
+
+                    <div className="col-lg-6">
+                      <div className="form-group">
+                        <label htmlFor="email">Email Address</label>
+
+                        <input
+                          type="email"
+                          className="form-control"
+                          name=""
+                          id="email"
+                        />
+                      </div>
+                    </div>
+
+                    <div className="col-lg-6">
+                      <div className="form-group">
+                        <label htmlFor="country">Country</label>
+
+                        <input
+                          type="url"
+                          className="form-control"
+                          name=""
+                          id="country"
+                        />
+                      </div>
+                    </div>
+
+                    <div className="col-lg-6">
+                      <div className="form-group">
+                        <label htmlFor="state">State</label>
+
+                        <input
+                          type="url"
+                          className="form-control"
+                          name=""
+                          id="state"
+                        />
+                      </div>
+                    </div>
                   </div>
-                </div>
 
-                <div className="col-lg-6">
-                  <div className="form-group">
-                    <label htmlFor="email">Email Address</label>
+                  <button
+                    type="button"
+                    className="btn-cta"
+                    onClick={handleNext}
+                  >
+                    Continue
+                  </button>
+                </>
+              )}
 
-                    <input
-                      type="email"
-                      className="form-control"
-                      name=""
-                      id="email"
-                    />
+              {step === 2 && (
+                <>
+                  <button onClick={handlePrev} className="block mb-5">
+                    &laquo; Previous
+                  </button>
+
+                  <h3 className="font-bold mb-3 text-lg">Social Media</h3>
+
+                  <div className="row row-30">
+                    <div className="col-lg-6">
+                      <div className="form-group">
+                        <label htmlFor="facebook">Facebook</label>
+
+                        <input
+                          type="url"
+                          className="form-control"
+                          name="facebook"
+                          id="facebook"
+                        />
+                      </div>
+                    </div>
+
+                    <div className="col-lg-6">
+                      <div className="form-group">
+                        <label htmlFor="twitter">Twitter</label>
+
+                        <input
+                          type="url"
+                          className="form-control"
+                          name="twitter"
+                          id="twitter"
+                        />
+                      </div>
+                    </div>
+
+                    <div className="col-lg-6">
+                      <div className="form-group">
+                        <label htmlFor="telegram">telegram</label>
+
+                        <input
+                          type="url"
+                          className="form-control"
+                          name="telegram"
+                          id="telegram"
+                        />
+                      </div>
+                    </div>
+
+                    <div className="col-lg-6">
+                      <div className="form-group">
+                        <label htmlFor="discord">Discord</label>
+
+                        <input
+                          type="url"
+                          className="form-control"
+                          name="discord"
+                          id="discord"
+                        />
+                      </div>
+                    </div>
+
+                    <div className="col-lg-6">
+                      <div className="form-group">
+                        <label htmlFor="instagram">Instagram</label>
+
+                        <input
+                          type="url"
+                          className="form-control"
+                          name="instagram"
+                          id="instagram"
+                        />
+                      </div>
+                    </div>
+
+                    <div className="col-lg-6">
+                      <div className="form-group">
+                        <label htmlFor="whatsapp">Whatsapp</label>
+
+                        <input
+                          type="url"
+                          className="form-control"
+                          name="whatsapp"
+                          id="whatsapp"
+                        />
+                      </div>
+                    </div>
                   </div>
-                </div>
 
-                <div className="col-lg-6">
-                  <div className="form-group">
-                    <label htmlFor="wallet">Wallet Address</label>
-
-                    <input
-                      type="text"
-                      className="form-control"
-                      name=""
-                      id="wallet"
-                    />
-                  </div>
-                </div>
-
-                <div className="col-lg-6">
-                  <div className="form-group">
-                    <label htmlFor="phone">Phone no</label>
-
-                    <input
-                      type="tel"
-                      className="form-control"
-                      name=""
-                      id="phone"
-                    />
-                  </div>
-                </div>
-              </div>
-
-              <div className="contact-message">
-                <label htmlFor="message">Wallet type</label>
-
-                <select name="" id="" className="form-control">
-                  <option value="DEX">DEX</option>
-                  <option value="CEX">CEX</option>
-                </select>
-              </div>
-
-              <button className="btn-cta">Add new Profile</button>
-            </form>
-          </div>
+                  <button className="btn-cta">Add new profile</button>
+                </>
+              )}
+            </div>
+          </form>
         </div>
       </div>
     </section>
