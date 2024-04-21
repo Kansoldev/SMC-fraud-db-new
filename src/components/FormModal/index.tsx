1780fbea89d5d759fde352878cec7bf55fd9ac42
oyinkansola <yahayaoyinkansola@gmail.com>
2024-04-21 04:52:01 +0100

    Add form modal component to add profile information

---
+++ src/components/FormModal/index.tsx
@@ -0,0 +1,34 @@
+import WalletForm from "@/src/view/dashboard/forms/addWallet";
+import ImageForm from "@/src/view/dashboard/forms/addImages";
+
+function FormModal({
+  isOpen,
+  currentBtn,
+  onClose,
+}: {
+  isOpen: Boolean;
+  currentBtn: { current: string };
+  onClose: () => void;
+}) {
+  return (
+    <div
+      className={`fixed inset-0 bg-gray-900 bg-opacity-50 flex justify-center items-center ${
+        isOpen ? "" : "hidden"
+      }`}
+    >
+      <div className="bg-white p-6 rounded-lg shadow-md">
+        <button
+          onClick={onClose}
+          className="text-white bg-black text-right px-2 mb-7 text-lg"
+        >
+          &times;
+        </button>
+
+        {currentBtn.current === "Add a new wallet" && <WalletForm />}
+        {currentBtn.current === "Add Images" && <ImageForm />}
+      </div>
+    </div>
+  );
+}
+
+export default FormModal;
